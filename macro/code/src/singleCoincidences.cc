6eddf2a9640ba75bf7851ff8a966bd762c71c290
irenecortinovis <irenecortinovis1@gmail.com>
2019-06-11 21:18:50 +0200

    Fixed globalposZ bug

--- macro/code/src/singleCoincidences.cc
+++ macro/code/src/singleCoincidences.cc
@@ -16,6 +16,36 @@ void singleCoincidences::FillSingle(std::vector<Hits::CoincidenceEvent> cvector)
 
   for(Long64_t i=0; i<cvector.size(); i++)
   {
+    //default values - not used in recostruction
+    runID = 0;
+    axialPos = 0;
+    sourceID1 = 0;
+    sourcePosX1 = 0;
+    sourcePosY1 = 0;
+    sourcePosZ1 = 0;
+    gantryID1 = 0;
+    moduleID1 = 0;
+    layerID1 = 0;
+    comptonCrystal1 = 0;
+    RayleighPhantom1 = 0;
+    RayleighCrystal1 = 0;
+    sourceID2 = 0;
+    sourcePosX2 = 0;
+    sourcePosY2 = 0;
+    sourcePosZ2 = 0;
+    gantryID2 = 0;
+    moduleID2 = 0;
+    layerID2 = 0;
+    comptonCrystal2 = 0;
+    RayleighPhantom2 = 0;
+    RayleighCrystal2 = 0;
+    sinogramTheta = 0;
+    sinogramS = 0;
+    comptVolName1[30] = '0';
+    comptVolName2[30] = '0';
+    RayleighVolName1[30] = '0';
+    RayleighVolName2[30] = '0';
+    
     //fill the thing using default values for useless, and cvector values for useful
     rotationAngle = (cvector.at(i)).rotationAngle;
     eventID1 = (cvector.at(i)).eventID1;
@@ -39,41 +69,7 @@ void singleCoincidences::FillSingle(std::vector<Hits::CoincidenceEvent> cvector)
     time1 = (cvector.at(i)).time1;
     time2 = (cvector.at(i)).time2;
 
-    //default values - not used in recostruction
-    runID = 0;
-    axialPos = 0;
-    sourceID1 = 0;
-    sourcePosX1 = 0;
-    sourcePosY1 = 0;
-    sourcePosZ1 = 0;
-    globalPosZ1 = 0;
-    gantryID1 = 0;
-    rsectorID1 = 0;
-    moduleID1 = 0;
-    submoduleID1 = 0;
-    layerID1 = 0;
-    comptonCrystal1 = 0;
-    RayleighPhantom1 = 0;
-    RayleighCrystal1 = 0;
-    sourceID2 = 0;
-    sourcePosX2 = 0;
-    sourcePosY2 = 0;
-    sourcePosZ2 = 0;
-    globalPosZ2 = 0;
-    gantryID2 = 0;
-    rsectorID2 = 0;
-    moduleID2 = 0;
-    submoduleID2 = 0;
-    layerID2 = 0;
-    comptonCrystal2 = 0;
-    RayleighPhantom2 = 0;
-    RayleighCrystal2 = 0;
-    sinogramTheta = 0;
-    sinogramS = 0;
-    comptVolName1[30] = '0';
-    comptVolName2[30] = '0';
-    RayleighVolName1[30] = '0';
-    RayleighVolName2[30] = '0';
+
 
     //fill entry
     fChain->Fill();
